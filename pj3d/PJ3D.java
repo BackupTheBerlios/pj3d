389a970de37306d88bf2086430e5ab23b60a399f
dk05 <dk05>
2004-11-02 13:50:06 +0000

    Kommentare eingefügt

--- pj3d/PJ3D.java
+++ pj3d/PJ3D.java
@@ -9,21 +9,34 @@ import com.sun.j3d.utils.behaviors.keyboard.KeyNavigatorBehavior;
 import com.sun.j3d.utils.universe.*;
 import com.sun.j3d.utils.geometry.*;
 
+///
+/// Enthält alle Unterobjekte die zum späteren Gebrauch in Processing benötigt werden.
+/// Die Unterobjekte: z.B. PJ3DBox oder  PJ3DSphere... werden bei der ersten 
+/// Initialisierung mit Referenziert. Blah...
+///
 public class PJ3D extends Applet
 {
 	private int mWinHeight, mWinWidth; 
 	private MainBranch mb;
 	private BranchGroup branch;
 	private float mColorBgWorldR, mColorBgWorldG, mColorBgWorldB;
-	
-	public Color3f backgroundColor, ambientColor, diffuseColor, emissiveColor, specularColor;
+	/// Hier kann eine Erklärung der eizelnen Werte rein, was sicherlich nicht schlecht ist fuer eine Processing user.
+	public Color3f backgroundColor, ambientColor, diffuseColor, emissiveColor, specularColor;	
+	/// Hier kann eine Erklärung der eizelnen Werte rein, was sicherlich nicht schlecht ist fuer eine Processing user. 
 	public float shininess, alpha;
+	/// Jaja, die Default Color
 	public final float DEFAULTCOLOR = 0.5f;
 	
 	private Vector3f transformVec;
 	
+	///
+	/// Standart Konstruktor.
+	///
 	public PJ3D(){}
 	
+	///
+	/// Benötigt als Werte die gewünschte Fenstergrösse.
+	///
 	public PJ3D(int windowWidth, int windowHeight)
 	{
 		this.mWinHeight = windowHeight;
@@ -32,28 +45,9 @@ public class PJ3D extends Applet
 		InitPJ3D(mWinWidth, mWinHeight);
 	}
 	
-	/*
-	public PJ3D(int windowWidth, int windowHeight)
-	{
-		this.mWinHeight = windowHeight;
-		this.mWinWidth = windowWidth;
-		this.mColorBgWorldR = colorBgWorldR;
-		this.mColorBgWorldG = colorBgWorldG;
-		this.mColorBgWorldB = colorBgWorldB;
-		
-		InitPJ3D(mWinWidth, mWinHeight);
-	}
-	*/
-	/*
-	public void init()
-	{
-		System.out.println("huhu");
-		BranchGroup hu = InitPJ3D(640, 480);
-		SimpleBox b = simpleBox(0.3f, 0.0f, 0.0f, 0.0f);
-		b.RotateObj(0.0, 0.3, 0.0);
-	}
-	*/
-	
+	///
+	/// Initialisierung der äußeren BranchGroup.
+	///
 	public BranchGroup InitPJ3D(int mWinWidth, int mWinHeight)
 	{		
 		setLayout( new BorderLayout( ) );
@@ -81,50 +75,46 @@ public class PJ3D extends Applet
 	    return branch;
 	}
 	
+	///
+	/// Erstellung eines Quadrates. Benötigt als Werte die Länge, Höhe und Breite des 
+	/// gewünschten Quadrates.
+	///
 	public PJ3DBox PJ3DBox( float xdim, float ydim, float zdim )
 	{
 		PJ3DBox s = new PJ3DBox (branch, xdim, ydim, zdim, transformVec, ambientColor, diffuseColor, emissiveColor, specularColor, shininess, alpha);
 		return s;
 	}
 	
+	///
+	/// Erstellt einen Würfel.
+	///
 	public PJ3DBox PJ3DBox( float xdim )
 	{
 		PJ3DBox s = new PJ3DBox (branch, xdim, xdim, xdim, transformVec, ambientColor, diffuseColor, emissiveColor, specularColor, shininess, alpha);
 		return s;
 	}
 	
+	///
+	/// Erstellt eine Sphere.
+	///
 	public PJ3DSphere PJ3DSphere( float xdim )
 	{
 		PJ3DSphere s = new PJ3DSphere (branch, xdim, transformVec, ambientColor, diffuseColor, emissiveColor, specularColor, shininess, alpha);
 		return s;
 	}
 	
+	///
+	/// Erstellt ein .x Objekt?? Welche Formate kann man da angeben?
+	///
 	public PJ3DObj PJ3DObj( String fileLocation )
 	{
 		PJ3DObj s = new PJ3DObj (branch, fileLocation, transformVec);
 		return s;
 	}
 	
-	/*
-	public PJ3DBox PJ3DBox( float xdim, float ydim, float zdim, float xpos, float ypos, float zpos )
-	{
-		PJ3DBox s = new PJ3DBox (branch, xdim, ydim, zdim, xpos, ypos, zpos);
-		return s;
-	}
-
-	public PJ3DBox PJ3DBox( float xdim, float ydim, float zdim, float xpos, float ypos, float zpos, float r, float g, float b )
-	{
-		PJ3DBox s = new PJ3DBox (branch, xdim, ydim, zdim, xpos, ypos, zpos, r, g, b);
-		return s;
-	}
-	
-	public PJ3DBox PJ3DBox( float xdim, float ydim, float zdim, float xpos, float ypos, float zpos, float r, float g, float b, float a )
-	{
-		PJ3DBox s = new PJ3DBox (branch, xdim, ydim, zdim, xpos, ypos, zpos, r, g, b, a);
-		return s;
-	}
-	*/
-	
+	///
+	/// Bestimmung der Hintergrundfarbe
+	///
 	public void setBackgroundColor(float r, float g, float b)
 	{
 		backgroundColor.x = r;
@@ -133,6 +123,9 @@ public class PJ3D extends Applet
 		mb.background.setColor(r, g, b);
 	}
 	
+	///
+	/// Bestimmung der Emissivefarbe
+	///		
 	public void setAmbientColor(float r, float g, float b)
 	{
 		ambientColor.x = r;
@@ -140,13 +133,19 @@ public class PJ3D extends Applet
 		ambientColor.z = b;
 	}
 	
+	///
+	/// Bestimmung der Diffusefarbe
+	///		
 	public void setDiffuseColor(float r, float g, float b)
 	{
 		diffuseColor.x = r;
 		diffuseColor.y = g;
 		diffuseColor.z = b;
 	}
-	
+
+	///
+	/// Bestimmung der Emissivefarbe
+	///	
 	public void setEmissiveColor(float r, float g, float b)
 	{
 		emissiveColor.x = r;
@@ -154,6 +153,9 @@ public class PJ3D extends Applet
 		emissiveColor.z = b;
 	}
 	
+	///
+	/// Bestimmung der Emissivefarbe
+	///		
 	public void setSpecularColor(float r, float g, float b)
 	{
 		specularColor.x = r;
@@ -161,16 +163,25 @@ public class PJ3D extends Applet
 		specularColor.z = b;
 	}
 	
+	///
+	/// Bestimmung der Shininess?
+	///		
 	public void setShininess(float s)
 	{
 		shininess = s;
 	}
-	
+
+	///
+	/// Bestimmung der Alphakanal stärke.
+	///	
 	public void setAlpha(float a)
 	{
 		alpha = a;
 	}
 	
+	///
+	/// Bestimmung der Emissivefarbe
+	///	
 	public void setTransform(float x, float y, float z)
 	{
 		transformVec.x = x;
